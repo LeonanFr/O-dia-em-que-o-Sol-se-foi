53688915fb7d7f36e3e8db760240c69d9e3959db
Iuky.O <iumyfarias03@gmail.com>
2025-09-22 19:25:34 -0300

    feat: puzzle closet

--- scripts/Closet.gd
+++ scripts/Closet.gd
@@ -3,10 +3,47 @@ extends InteractiveObject
 @export var focus_marker: Marker3D
 @export var animation_player: AnimationPlayer
 
-func _ready():
-	animation_player.play("RESET")
+@export var slots: Array[Node3D] = []
+@export var puzzle_objects: Array[Node3D] = []
+@export var correct_order: Array[int] = [5, 2, 0, 4, 1, 3]
 
-func get_focus_transform():
+var selected: Node3D = null
+var labels: Array[Label3D] = []
+
+func _ready() -> void:
+	animation_player.play("RESET")
+	add_to_group("closet_manager")
+	
+	slots = [
+		$Slot1, $Slot2, $Slot3,
+		$Slot4, $Slot5, $Slot6
+	]
+
+	puzzle_objects = [
+		$bag, #0
+		$shoes, #1
+		$frog, #2
+		$camisas, #3
+		$jeans, #4
+		$towel #5
+	]
+	#marrom, verde, azul, azul, vermelho, preto | towel, frog, bag, jeans, shoes, camisas
+
+	# Posiciona os objetos nos slots
+	for i in range(min(puzzle_objects.size(), slots.size())):
+		place_object_in_slot(puzzle_objects[i], slots[i])
+
+	labels = [
+		get_node_or_null("Label1"),
+		get_node_or_null("Label2"),
+		get_node_or_null("Label3"),
+		get_node_or_null("Label4"),
+	]
+	for label in labels:
+		if label:
+			label.hide()
+
+func get_focus_transform() -> Transform3D:
 	return focus_marker.global_transform if focus_marker else null
 
 func interact(action: String = "") -> void:
@@ -16,5 +53,51 @@ func interact(action: String = "") -> void:
 		elif action == "close":
 			animation_player.play("close")
 
-	if action != "close":
-		emit_signal("interacted", item_data.id)
+func on_item_clicked(obj: Node3D) -> void:
+	if selected == null:
+		selected = obj
+		print("Selecionado:", obj.name)
+	else:
+		print("Trocando:", selected.name, "<->", obj.name)
+		swap_objects(selected, obj)
+		selected = null
+		check_solution()
+
+func swap_objects(obj1: Node3D, obj2: Node3D) -> void:
+	var i1 = puzzle_objects.find(obj1)
+	var i2 = puzzle_objects.find(obj2)
+	if i1 == -1 or i2 == -1:
+		return
+
+	puzzle_objects[i1] = obj2
+	puzzle_objects[i2] = obj1
+
+	place_object_in_slot(obj1, slots[i2])
+	place_object_in_slot(obj2, slots[i1])
+
+func place_object_in_slot(obj: Node3D, slot: Node3D) -> void:
+	obj.global_position = slot.global_position
+	obj.global_rotation = slot.global_rotation
+
+func check_solution() -> void:
+	var current: Array[int] = []
+	for obj in puzzle_objects:
+		if obj.has_method("get"):
+			current.append(obj.item_id)
+
+	print("Current IDs:", current, "Expected IDs:", correct_order)
+
+	if current == correct_order:
+		puzzle_solved()
+
+func puzzle_solved() -> void:
+	print("Closet puzzle resolvido!")
+
+	for obj in puzzle_objects:
+		obj.hide()
+
+	var numeros = ["7", "2", "9", "4"]
+	for i in range(labels.size()):
+		if labels[i]:
+			labels[i].text = numeros[i]
+			labels[i].show()
---
+++ scripts/camisas.gd
@@ -0,0 +1,8 @@
+extends InteractiveObject
+
+@export var item_id: int = 3
+
+func interact(action: String = "") -> void:
+	var closet_manager = get_tree().get_first_node_in_group("closet_manager")
+	if closet_manager:
+		closet_manager.on_item_clicked(self)
